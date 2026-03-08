adda67a47fa42e2d00560469eb4ca0f3645b1e4f
GiladMoalem <giladmoa@gmail.com>
2026-03-09 01:01:51 +0200

    first commit - export to a excel file

---
+++ main.py
@@ -0,0 +1,114 @@
+import requests
+import time
+import pandas as pd
+
+BASE = "https://www.izkor.gov.il"
+
+session = requests.Session()
+
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "application/json",
+    "Cookie":  # EDIT THE COOKIES
+        "visid_incap_1068888=smzJnPfASxSjP4wS1b8eUXxYpGkAAAAAQUIPAAAAAACba8GrGXs9alnI4ibuB6bE; nlbi_1068888=5fu9YTnpZkcEsg+hT/Ry9AAAAACvWJWOFB396Ukm5ZIWWr7x; incap_ses_7213_1068888=/TnrD3RPiS6A+rdkzbkZZMX6rWkAAAAAyKKG5QiyXikkpr6fXuKquA=="
+}
+
+
+def get_cemeteries():
+    url = f"{BASE}/search/list-fields"
+    r = session.get(url, headers=headers)
+    r.raise_for_status()
+
+    # print(r.text)
+    data = r.json()
+    return data["cemeteriesSearch"]["data"]
+
+
+def get_fallen(cemetery):
+    url = f"{BASE}/search/extended"
+
+    page = 0
+    results = []
+
+    while True:
+
+        payload = {
+            "cemetery": cemetery,
+            "page": page
+        }
+
+        r = session.post(url, headers=headers, json=payload)
+        r.raise_for_status()
+
+        data = r.json()["data"]
+
+        if not data:
+            break
+
+        results.extend(data)
+
+        print(f"{cemetery['name']} page {page} -> {len(data)}")
+
+        page += 1
+        time.sleep(0.3)
+
+    return results
+
+
+def filter_cemeteries(cemeteries, filters):
+
+    filtered = []
+
+    for cemetery in cemeteries:
+
+        name = cemetery.get("name", "")
+
+        for f in filters:
+            if f in name:
+                filtered.append(cemetery)
+                break
+
+    return filtered
+
+
+def load_filters(filename="filter_cities.txt"):
+    with open(filename, encoding="utf8") as f:
+        return [line.strip() for line in f if line.strip()]
+
+
+def save_to_excel_file(all_fallen):
+    df = pd.DataFrame(all_fallen)
+    df.to_excel("izkor_data.xlsx", index=False)
+
+
+def main():
+
+    cemeteries = get_cemeteries()
+
+    filters = load_filters()
+
+    cemeteries = filter_cemeteries(cemeteries, filters)
+
+    print(f"cemeteries list after filter: {[cemetery['name'] for cemetery in cemeteries]}")
+    all_fallen = []
+
+    for cemetery in cemeteries:
+
+        if cemetery["legacy_id"] is None:
+            continue
+
+        print("Fetching", cemetery["name"])
+
+        fallen = get_fallen(cemetery)
+        for one_fallen in fallen:
+            one_fallen["cemetery"] = cemetery['name']
+
+        all_fallen.extend(fallen)
+
+    print("Total:", len(all_fallen))
+
+    save_to_excel_file(all_fallen)
+
+
+if __name__ == "__main__":
+    main()
